d0387f2f2f31eb094ea61abcb6668550a8399218
arthuro10 <81263142+arthuro10@users.noreply.github.com>
2022-05-25 12:07:56 +0200

    learning_ts

---
+++ aFirstApp/index_ts_syntax.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// typescript syntax
+// interface --> die Struktur eines Objekts definieren
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then((response) => {
+    //console.log(response.data);
+
+    const todo = response.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    logTodo(id, completed, title);
+    //logTodo(id, title, completed);
+    
+
+});
+
+const logTodo = (id: number, completed: boolean, title: string)  => {
+    console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+    `)
+}
+
+
+
